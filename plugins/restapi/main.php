4fa31f38337a55a20a01780b866c6cf9d338cd66
Sam Tuke <mail@samtuke.com>
2015-01-08 15:09:44 +0100

    Updated docs, structure, and attribution

--- plugins/restapi/main.php
+++ plugins/restapi/main.php
@@ -12,20 +12,20 @@ $url = Common::apiUrl( $website );
 
 ?>
 
-<h1>RESTAPI</h1>
+<h1>REST API</h1>
 
     <h2>Version 0.2.5</h2>
-    <p>The plugin provides a REST API to phpList.<br/>
-    Development by Flowcom AB, Andreas Ek (<a href="https://twitter.com/ekandreas">@EkAndreas</a>)</p>
+    <p>The plugin provides a REST API to phpList.<br/> Development by <a href="http://samtuke.com">Sam Tuke, <a href="http://phplist.com">Michiel Dethmers</a>. Original development by <a href="https://twitter.com/ekandreas">Andreas Ek</a> of Flowcom AB.</p>
 
+    <h2>Commands</h2>
     <p>
-        <h2>Commands</h2>
         To discover all commands to this API just make a GET request or click here:<br/>
         <a href="<?php echo $url; ?>">phpList API Command Reference list</a><br/>
         The documentation is generated in realtime.
     </p>
+
+    <h2>Access</h2>
     <p>
-        <h2>Access</h2>
         Autentication required as admin in phpList.<br/>
         All requests to the RESTAPI is made by method POST.<br/>
         RESTAPI-Url to this installation:<br/>
@@ -34,22 +34,20 @@ $url = Common::apiUrl( $website );
     <p>
         First login to phpList with method POST and body parameters "login" and "password".<br/>
     </p>
+
+    <h2>Client</h2>
     <p>
-        <h2>Client</h2>
         To try the RESTAPI, please use a client like CocaRestClient or eqvivalent!<br/>
         There is an example class in restapi-test/phplist_restapi_helper.php if you like to try it in PHP.<br/>
         For examples check commands in restapi-test/main.php
     </p>
 
+    <h2>More information</h2>
     <p>
-        <h2>More information</h2>
-        Please read more at Github!
-        <a href="https://github.com/EkAndreas/phplistapi">https://github.com/EkAndreas/phplistapi</a>
+        See the readme file in this plugin's root directory for further instructions (<code>README.md</code>).
     </p>
 
+    <h2>Issues</h2>
     <p>
-        <h2>Issues</h2>
-        All issues regarding the RESTAPI is handled at Github!
-        <a href="https://github.com/EkAndreas/phplistapi">https://github.com/EkAndreas/phplistapi</a>
+        Report issues to the central phpList <a href="https://mantis.phplist.com/">bug tracker</a>.
     </p>
-

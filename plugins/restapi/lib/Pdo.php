0f0250684883fda4d355f1b9728c0091e692b071
Sam Tuke <mail@samtuke.com>
2015-01-09 17:37:27 +0100

    Renamed lib files to PSR-4 names; Fixed quotes in Pdo{}

--- plugins/restapi/lib/Pdo.php
+++ plugins/restapi/lib/Pdo.php
@@ -12,7 +12,7 @@ class Pdo extends \PDO {
         $dbuser = $GLOBALS['database_user'];
         $dbpass = $GLOBALS['database_password'];
         $dbname = $GLOBALS['database_name'];
-        $dbh = new \PDO('mysql:host=$dbhost;dbname=$dbname', $dbuser, $dbpass);
+        $dbh = new \PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
         $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $dbh;
     }

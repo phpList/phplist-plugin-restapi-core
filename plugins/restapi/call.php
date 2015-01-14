8dfb6e14bfa8b6b04b101d19c8f0b87875b95845
Sam Tuke <mail@samtuke.com>
2015-01-14 17:50:43 +0100

    Improved structure; Added comments

--- plugins/restapi/call.php
+++ plugins/restapi/call.php
@@ -21,23 +21,26 @@ include 'includes/subscribers.php';
 include 'includes/templates.php';
 include 'includes/messages.php';
 
-if (function_exists('api_request_log')) {
-  api_request_log();
+if (function_exists('api_request_log'))
+{
+    api_request_log();
 }
 
-//Check if this is called outside phpList auth, this should never occur!
-if ( empty( $plugin->coderoot ) ){
+// Check if this is called outside phpList auth, this should never occur!
+if ( empty( $plugin->coderoot ) )
+{
     Response::outputErrorMessage( 'Not authorized! Please login with [login] and [password] as admin first!' );
 }
 
-//Check if command is empty!
+// Check if command is empty!
 $cmd = $_REQUEST['cmd'];
 $cmd = preg_replace('/\W/','',$cmd);
 if ( empty($cmd) ){
-  Response::outputMessage('OK! For action, please provide Post Param Key [cmd] !');
+    Response::outputMessage('OK! For action, please provide Post Param Key [cmd] !');
 }
 
-//Now bind the commands with static functions
+// Try calling the requested method on all of the available classes
+// NOTE: This looks inefficient
 if ( is_callable( array( 'phpListRestapi\Lists',       $cmd ) ) ) Lists::$cmd();
 if ( is_callable( array( 'phpListRestapi\Actions',     $cmd ) ) ) Actions::$cmd();
 if ( is_callable( array( 'phpListRestapi\Subscribers', $cmd ) ) ) Subscribers::$cmd();

55c2c69f6d61815ec6f7a99e667fcb1e4687813e
Sam Tuke <mail@samtuke.com>
2015-01-17 21:06:55 +0100

    Instantiated new necessary objects; Ported it to new non-static class interfaces

--- plugins/restapi/call.php
+++ plugins/restapi/call.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace phpListRestapi;
+namespace Rapi;
+
+require_once 'vendor/autoload.php';
 
 // Check that the plugin has been initiatlised
 defined('PHPLISTINIT') || die;
@@ -17,32 +19,33 @@ if (function_exists('api_request_log'))
 }
 
 $response = new Response();
-$common = new Common();
+$pdoEx = new PdoEx();
+$common = new Common( $pdoEx );
 $actions = new Actions( $response );
-$subscribers = new Subscribers();
+$subscribers = new Subscribers( $common, $pdoEx, $response );
 $templates = new Templates( $common );
 $messages = new Messages();
 
 // Check if this is called outside phpList auth, this should never occur!
 if ( empty( $plugin->coderoot ) )
 {
-    Response::outputErrorMessage( 'Not authorized! Please login with [login] and [password] as admin first!' );
+    $response->outputErrorMessage( 'Not authorized! Please login with [login] and [password] as admin first!' );
 }
 
 // Check if command is empty!
 $cmd = $_REQUEST['cmd'];
-$cmd = preg_replace('/\W/','',$cmd);
+$cmd = preg_replace( '/\W/','',$cmd );
 if ( empty($cmd) ){
-    Response::outputMessage('OK! For action, please provide Post Param Key [cmd] !');
+    $response->outputMessage( 'OK! For action, please provide Post Param Key [cmd] !' );
 }
 
 // Try calling the requested method on all of the available classes
 // NOTE: This looks inefficient
-if ( is_callable( array( 'phpListRestapi\Lists',       $cmd ) ) ) Lists::$cmd();
-if ( is_callable( array( 'phpListRestapi\Actions',     $cmd ) ) ) Actions::$cmd();
-if ( is_callable( array( 'phpListRestapi\Subscribers', $cmd ) ) ) Subscribers::$cmd();
-if ( is_callable( array( 'phpListRestapi\Templates',   $cmd ) ) ) Templates::$cmd();
-if ( is_callable( array( 'phpListRestapi\Messages',    $cmd ) ) ) Messages::$cmd();
+if ( is_callable( array( 'Rapi\Lists',       $cmd ) ) ) $lists->$cmd();
+if ( is_callable( array( 'Rapi\Actions',     $cmd ) ) ) $actions->$cmd();
+if ( is_callable( array( 'Rapi\Subscribers', $cmd ) ) ) $subscribers->$cmd();
+if ( is_callable( array( 'Rapi\Templates',   $cmd ) ) ) $templates->$cmd();
+if ( is_callable( array( 'Rapi\Messages',    $cmd ) ) ) $messages->$cmd();
 
 //If no command found, return error message!
-Response::outputErrorMessage( 'No function for provided [cmd] found!' );
+$response->outputErrorMessage( 'No function for provided [cmd] found!' );

1ea49191b3d59d59010955edaf1e6345a61407b2
Sam Tuke <mail@samtuke.com>
2015-01-23 17:40:30 +0100

    Ported call.php to use new DI services container

--- plugins/restapi/call.php
+++ plugins/restapi/call.php
@@ -1,6 +1,10 @@
 <?php
 
-namespace Rapi;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Rapi;
 
 require_once 'vendor/autoload.php';
 
@@ -13,18 +17,35 @@ ob_end_clean();
 // Getting phpList globals for this plugin
 $plugin = $GLOBALS['plugins'][$_GET['pi']];
 
+// Create Symfony DI service container object for use by other classes
+$container = new ContainerBuilder();
+// Create new Symfony file loader to handle the YAML service config file
+$loader = new YamlFileLoader( $container, new FileLocator(__DIR__) );
+// Load the service config file, which is in YAML format
+$loader->load( 'services.yml' );
+
 if (function_exists('api_request_log'))
 {
     api_request_log();
 }
 
-$response = new Response();
-$pdoEx = new PdoEx();
-$common = new Common( $pdoEx );
-$actions = new Actions( $response );
-$subscribers = new Subscribers( $common, $pdoEx, $response );
-$templates = new Templates( $common );
-$messages = new Messages();
+// Instantiate objects for use
+$pdoEx = $container->get( 'PdoEx' );
+$response = $container->get( 'Response' );
+$common = $container->get( 'Common' );
+$actions = $container->get( 'Actions' );
+$subscribers = $container->get( 'Subscribers' );
+$templates = $container->get( 'Templates' );
+$messages = $container->get( 'Messages' );
+$lists = $container->get( 'Lists' );
+
+// Connect to database
+$pdoEx->connect(
+    $GLOBALS['database_host']
+    , $GLOBALS['database_user']
+    , $GLOBALS['database_password']
+    , $GLOBALS['database_name']
+);
 
 // Check if this is called outside phpList auth, this should never occur!
 if ( empty( $plugin->coderoot ) )

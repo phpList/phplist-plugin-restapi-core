346daa54cabb0095668faa10d2749a519e78f464
Sam Tuke <mail@samtuke.com>
2015-01-27 18:55:21 +0100

    Started porting to non-static

--- plugins/restapi/lib/Messages.php
+++ plugins/restapi/lib/Messages.php
@@ -8,15 +8,15 @@ defined('PHPLISTINIT') || die;
  * Class Messages
  * Manage phplist Messages
  */
-class Messages{
+class Messages {
 
-    static function messageGet( $id=0 ) {
+    public function messageGet( $id=0 ) {
         if ( $id==0 ) $id = $_REQUEST['id'];
-        Common::select( 'Message', "SELECT * FROM " . $GLOBALS['table_prefix'] . "message WHERE id=" . $id . ";", true );
+        Common->select( 'Message', "SELECT * FROM " . $GLOBALS['table_prefix'] . "message WHERE id=" . $id . ";", true );
     }
 
-    static function messagesGet() {
-        Common::select( 'Messages', "SELECT * FROM " . $GLOBALS['table_prefix'] . "message ORDER BY Modified DESC;" );
+    public function messagesGet() {
+        Common->select( 'Messages', "SELECT * FROM " . $GLOBALS['table_prefix'] . "message ORDER BY Modified DESC;" );
     }
 
     /**
@@ -39,11 +39,12 @@ class Messages{
      * The message added.
      *
      */
-    static function messageAdd(){
+    public function messageAdd()
+	{
 
         $sql = "INSERT INTO " . $GLOBALS['table_prefix'] . "message (subject, fromfield, replyto, message, textmessage, footer, entered, status, sendformat, template, embargo, rsstemplate, owner, htmlformatted ) VALUES ( :subject, :fromfield, :replyto, :message, :textmessage, :footer, now(), :status, :sendformat, :template, :embargo, :rsstemplate, :owner, :htmlformatted );";
         try {
-            $db = PDO::getConnection();
+            $db = PDO->getConnection();
             $stmt = $db->prepare($sql);
             $stmt->bindParam("subject", $_REQUEST['subject'] );
             $stmt->bindParam("fromfield", $_REQUEST['fromfield'] );
@@ -61,9 +62,9 @@ class Messages{
             $stmt->execute();
             $id = $db->lastInsertId();
             $db = null;
-            Messages::messageGet( $id );
+            $this->messageGet( $id );
         } catch(\PDOException $e) {
-            Response::outputError($e);
+            Response->outputError($e);
         }
 
     }
@@ -89,12 +90,13 @@ class Messages{
      * The message added.
      *
      */
-    static function messageUpdate( $id = 0 ){
+    public function messageUpdate( $id = 0 )
+	{
 
         if ( $id == 0 ) $id = $_REQUEST['id'];
         $sql = "UPDATE " . $GLOBALS['table_prefix'] . "message SET subject=:subject, fromfield=:fromfield, replyto=:replyto, message=:message, textmessage=:textmessage, footer=:footer, status=:status, sendformat=:sendformat, template=:template, sendstart=:sendstart, rsstemplate=:rsstemplate, owner=:owner, htmlformatted=:htmlformatted WHERE id=:id;";
         try {
-            $db = PDO::getConnection();
+            $db = PDO->getConnection();
             $stmt = $db->prepare($sql);
             $stmt->bindParam("id", $id );
             $stmt->bindParam("subject", $_REQUEST['subject'] );
@@ -112,14 +114,15 @@ class Messages{
             $stmt->bindParam("htmlformatted", $_REQUEST['htmlformatted'] );
             $stmt->execute();
             $db = null;
-            Messages::messageGet( $id );
+            $this->messageGet( $id );
         } catch(\PDOException $e) {
-            Response::outputError($e);
+            Response->outputError($e);
         }
 
     }
 
-	static function formtokenGet(){
+	public function formtokenGet()
+	{
 		$key = md5(time().mt_rand(0,10000));
 		Sql_Query(sprintf('insert into %s (adminid,value,entered,expires) values(%d,"%s",%d,date_add(now(),interval 1 hour))',
 		$GLOBALS['tables']['admintoken'],$_SESSION['logindetails']['id'],$key,time()),1);

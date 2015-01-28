c6b35a822f34a8e7ed5c8496b749b617ae3aeec9
Sam Tuke <mail@samtuke.com>
2015-01-28 18:40:16 +0100

    Moved connection logic into constructor - service container can now pass this data successfully

--- plugins/restapi/lib/PdoEx.php
+++ plugins/restapi/lib/PdoEx.php
@@ -8,14 +8,11 @@ namespace Rapi;
  */
 class PdoEx extends \PDO {
 
-    public function __construct()
+    public function __construct( $hostname, $username, $pass, $dbname )
     {
-        // NOTE: This constructor is here to avoid automatic call to the parent
-        // constructor of PDF, which requires parameters
-    }
+        // NOTE: If this constructor isn't present, the parent class'
+        // constructor will automatically be called.
 
-    public function connect( $hostname, $username, $pass, $dbname )
-    {
         // Create a PDO object
         parent::__construct( "mysql:host=$hostname;dbname=$username", $pass, $dbname );
 

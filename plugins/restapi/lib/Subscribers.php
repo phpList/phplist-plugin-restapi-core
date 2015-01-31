8e9c49fc5a6cc72e03e8ed3123002cf48e86ea0a
Sam Tuke <mail@samtuke.com>
2015-01-31 17:57:19 +0100

    Improved formatting

--- plugins/restapi/lib/Subscribers.php
+++ plugins/restapi/lib/Subscribers.php
@@ -28,10 +28,10 @@ class Subscribers {
      */
     static function subscribersGet( $order_by='id', $order='asc', $limit=100 ) {
 
-				//getting optional values
-				if ( isset( $_REQUEST['order_by'] ) && !empty( $_REQUEST['order_by'] ) ) $order_by = $_REQUEST['order_by'];
-				if ( isset( $_REQUEST['order'] ) && !empty( $_REQUEST['order'] ) ) $order = $_REQUEST['order'];
-				if ( isset( $_REQUEST['limit'] ) && !empty( $_REQUEST['limit'] ) ) $limit = $_REQUEST['limit'];
+        //getting optional values
+        if ( isset( $_REQUEST['order_by'] ) && !empty( $_REQUEST['order_by'] ) ) $order_by = $_REQUEST['order_by'];
+        if ( isset( $_REQUEST['order'] ) && !empty( $_REQUEST['order'] ) ) $order = $_REQUEST['order'];
+        if ( isset( $_REQUEST['limit'] ) && !empty( $_REQUEST['limit'] ) ) $limit = $_REQUEST['limit'];
 
         $this->common->select( 'Users', 'SELECT * FROM ' . $GLOBALS['usertable_prefix'] . 'user ORDER BY $order_by $order LIMIT $limit;' );
     }
@@ -108,21 +108,21 @@ class Subscribers {
 
     }
 
-		/**
-		 * <p>Updates one Subscriber to the system.</p>
-		 * <p><strong>Parameters:</strong><br/>
-		 * [*id] {integer} the ID of the Subscriber.<br/>
-		 * [*email] {string} the email address of the Subscriber.<br/>
-		 * [*confirmed] {integer} 1=confirmed, 0=unconfirmed.<br/>
-		 * [*htmlemail] {integer} 1=html emails, 0=no html emails.<br/>
-		 * [*rssfrequency] {integer}<br/>
-		 * [*password] {string} The password to this Subscriber.<br/>
-		 * [*disabled] {integer} 1=disabled, 0=enabled<br/>
-		 * </p>
-		 * <p><strong>Returns:</strong><br/>
-		 * The updated Subscriber.
-		 * </p>
-		 */
+    /**
+        * <p>Updates one Subscriber to the system.</p>
+        * <p><strong>Parameters:</strong><br/>
+        * [*id] {integer} the ID of the Subscriber.<br/>
+        * [*email] {string} the email address of the Subscriber.<br/>
+        * [*confirmed] {integer} 1=confirmed, 0=unconfirmed.<br/>
+        * [*htmlemail] {integer} 1=html emails, 0=no html emails.<br/>
+        * [*rssfrequency] {integer}<br/>
+        * [*password] {string} The password to this Subscriber.<br/>
+        * [*disabled] {integer} 1=disabled, 0=enabled<br/>
+        * </p>
+        * <p><strong>Returns:</strong><br/>
+        * The updated Subscriber.
+        * </p>
+        */
     static function subscriberUpdate(){
 
         $sql = 'UPDATE ' . $GLOBALS['usertable_prefix'] . 'user SET email=:email, confirmed=:confirmed, htmlemail=:htmlemail, rssfrequency=:rssfrequency, password=:password, passwordchanged=now(), disabled=:disabled WHERE id=:id;';
@@ -146,15 +146,15 @@ class Subscribers {
 
     }
 
-		/**
-		 * <p>Deletes a Subscriber.</p>
-		 * <p><strong>Parameters:</strong><br/>
-		 * [*id] {integer} the ID of the Subscriber.<br/>
-		 * </p>
-		 * <p><strong>Returns:</strong><br/>
-		 * The deleted Subscriber ID.
-		 * </p>
-		 */
+    /**
+        * <p>Deletes a Subscriber.</p>
+        * <p><strong>Parameters:</strong><br/>
+        * [*id] {integer} the ID of the Subscriber.<br/>
+        * </p>
+        * <p><strong>Returns:</strong><br/>
+        * The deleted Subscriber ID.
+        * </p>
+        */
     static function subscriberDelete(){
 
         $sql = 'DELETE FROM ' . $GLOBALS['usertable_prefix'] . 'user WHERE id=:id;';

0a881850fe8e536d0a29e6c05067fb40ac63dc3a
Sam Tuke <mail@samtuke.com>
2015-03-10 19:44:54 +0100

    Updated tests and services to reflect latest phpList 4 code - unit tests passing

--- restapi/tests/P4Test.php
+++ restapi/tests/P4Test.php
@@ -2,10 +2,12 @@
 
 // Phplist 4 namespaces
 use phpList\Config;
-use phpList\EmailAddress;
-use phpList\entities\SubscriberEntity;
+use phpList\EmailUtil;
+use phpList\SubscriberManager;
+use phpList\Entity\SubscriberEntity;
+use phpList\Model\SubscriberModel;
 use phpList\helper\Database;
-use phpList\Password;
+use phpList\Pass;
 use phpList\phpList;
 use phpList\Subscriber;
 use phpList\helper\Util;
@@ -24,7 +26,9 @@ class Pl4Test extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         // Create a randomised email addy to register with
-        $this->emailAddress = $emailAddress = 'unittest-' . rand( 0, 999999 ) . '@example.com';
+        $this->emailAddress = 'unittest-' . rand( 0, 999999 ) . '@example.com';
+        $this->plainPass = 'easypassword';
+
         // Create Symfony DI service container object for use by other classes
         $this->container = new ContainerBuilder();
         // Create new Symfony file loader to handle the YAML service config file
@@ -35,44 +39,86 @@ class Pl4Test extends \PHPUnit_Framework_TestCase
         $this->container->setParameter( 'config.configfile', '/var/www/pl4/config.ini' );
         // Get objects from container
         $this->config = $this->container->get( 'Config' );
-        $this->subscriber = $this->container->get( 'Subscriber' );
-    }
 
-    public function testSave()
-    {
-        $emailCopy = $this->emailAddress;
-        $scrEntity = new SubscriberEntity(
-        new EmailAddress( $this->config, $this->emailAddress ),
-        new Password( $this->config, 'IHAVEANEASYPASSWORD' )
-    );
-    $this->subscriber->save( $scrEntity );
+        // Instantiate util classes
+        $this->emailUtil = new EmailUtil();
+        $this->pass = new Pass();
 
-    return array( 'id' => $scrEntity->id, 'email' => $emailCopy );
+        // Instantiate remaining classes
+        $this->db = new Database( $this->config );
+        $this->subscriberModel = new SubscriberModel( $this->config, $this->db );
+        $this->subscriberManager = new SubscriberManager(
+            $this->config
+            , $this->emailUtil
+            , $this->pass
+            , $this->subscriberModel
+        );
     }
 
     /**
-    * @depends testSave
-    * @param SubscriberEntity $scrEntity [description]
+    * @note This belongs in a test class for SubscriberEntity, not here
+    */
+    public function testAdd()
+    {
+        // Add new subscriber properties to the entity
+        $scrEntity = new SubscriberEntity( $this->emailAddress, $this->plainPass );
+        // Copy the email address to test it later
+        $emailCopy = $this->emailAddress;
+        // Save the subscriber
+        $newSubscriberId = $this->subscriberManager->add( $scrEntity );
+
+        // Test that an ID was returned
+        $this->assertNotEmpty( $newSubscriberId );
+        $this->assertTrue( is_numeric( $newSubscriberId ) );
+
+        // Pass on to the next test
+        return array( 'id' => $newSubscriberId, 'email' => $emailCopy, 'encPass' => $scrEntity->encPass );
+    }
+
+    /**
+    * @depends testAdd
     */
     public function testGetSubscriber( array $vars )
     {
-        $scrEntity = $this->subscriber->getSubscriber( $vars['id'] );
-        // Check that the saved passwords can be retrieved and are equal
-        $this->assertEquals(
-        $scrEntity->password->getEncryptedPassword()
-        , $scrEntity->password->getEncryptedPassword()
-    );
-    // Check that retrieved email matches what was set
-    $this->assertEquals(
-    $vars['email']
-    , $scrEntity->email_address->getAddress()
-    );
+        $scrEntity = $this->subscriberManager->getSubscriber( $vars['id'] );
 
-    // Delete the testing subscribers
-    // NOTE: These entities are used in other tests and must be deleted in
-    // whatever method users them last
-    $this->subscriber->delete( $vars['id'] );
+        // Check that the correct entity was returned
+        $this->assertInstanceOf( '\phpList\Entity\SubscriberEntity', $scrEntity );
+        // Check that the saved password isn't in plain text
+        $this->assertNotEquals( $this->plainPass, $scrEntity->encPass );
+        // Check that retrieved email matches what was set
+        $this->assertEquals( $vars['email'] , $scrEntity->emailAddress );
 
-    return $scrEntity;
+        return $scrEntity;
+    }
+
+    /**
+    * @depends testGetSubscriber
+    */
+    public function testUpdatePass( $scrEntity )
+    {
+        // Set a new password for testing
+        $newPlainPass = 'newEasyPassword';
+        // Update the password
+        $this->subscriberManager->updatePass( $newPlainPass, $scrEntity );
+        // Get a fresh copy of the subscriber from db to check updated details
+        $updatedScrEntity = $this->subscriberManager->getSubscriber( $scrEntity->id );
+
+        // Check that the passwords are not the same; that it was updated
+        $this->assertNotEquals( $scrEntity->encPass, $updatedScrEntity->encPass );
+    }
+
+    /**
+    * @depends testGetSubscriber
+    */
+    public function testDelete( $scrEntity )
+    {
+        // Delete the testing subscribers
+        // NOTE: These entities are used in other tests and must be deleted in
+        // whatever method users them last
+        $result = $this->subscriberManager->delete( $scrEntity->id );
+
+        // Check that delete was successful
+        $this->assertTrue( $result );
     }
 }

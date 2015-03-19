47ecb77f4c9de70bca6d589492878969d1b8b81d
Sam Tuke <mail@samtuke.com>
2015-03-19 12:52:45 +0100

    Switched order of arguments to fix ordering bug; Added new call handler methods

--- restapi/lib/Handler/SubscriberHandler.php
+++ restapi/lib/Handler/SubscriberHandler.php
@@ -15,11 +15,11 @@ class SubscriberHandler
       * @return int ID of new subscriber
       */
      public function add(
-        $emailAddress
-        , $blacklisted = 0
+        $blacklisted = 0
         , $bounceCount = 0
         , $confirmed = 1
         , $disabled = 0
+        , $emailAddress = "fdfs@dsadas.csa"
         , $encPass = ""
         , $entered = ""
         , $extraData = ""
@@ -48,10 +48,30 @@ class SubscriberHandler
 
      }
 
+     /**
+     * Insert a new subscriber with only an email address
+     * @return int ID of new subscriber
+     */
+     public function addEmailOnly( $emailAddress )
+     {
+         // Save the email address to the subscriber entity
+         $this->scrEntity->emailAddress = $emailAddress;
+
+         // Insert the subscriber & return
+         return $this->subscriberManager->add( $this->scrEntity );
+
+     }
+
      public function getById( $id )
      {
         // Get the subscriber & return
          return $this->subscriberManager->getSubscriberById( $id );
+     }
+
+     public function delete( $id )
+     {
+         // delete the subscriber and return
+         return $this->subscriberManager->delete( $id );
      }
 
 }

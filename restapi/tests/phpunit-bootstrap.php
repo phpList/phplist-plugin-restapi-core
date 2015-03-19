7893bf0d2671851a018e71c07f26f460f2351e3c
Sam Tuke <mail@samtuke.com>
2015-03-19 13:48:54 +0100

    Added Symfony DI container setup to bootstrap file to avoid duplication in tests

--- restapi/tests/phpunit-bootstrap.php
+++ restapi/tests/phpunit-bootstrap.php
@@ -1,17 +1,15 @@
 <?php
 
+// Symfony namespaces
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
 require '/var/www/pl/configdev.php';
 
 // Include Symfony autoloader
 require_once( 'vendor/autoload.php' );
-
-// TODO: Include setup file for dummy globals etc.
-$GLOBALS['usertable_prefix'] = 'phplist_';
-$GLOBALS['database_host'] = 'localhost';
-$GLOBALS['database_user'] = 'pl';
-$GLOBALS['database_password'] = 'pl';
-$GLOBALS['database_name'] = 'pl';
-
 
 // Include config file as it includes necessary config vars
 if (
@@ -20,3 +18,17 @@ if (
 ) {
     require_once( $_SERVER['ConfigFile'] );
 }
+
+// Create Symfony DI service container object for use by other classes
+$container = new ContainerBuilder();
+// Create new Symfony file loader to handle the YAML service config file
+$loader = new YamlFileLoader( $container, new FileLocator(__DIR__) );
+// Load the service config file, which is in YAML format
+$loader->load( '../services.yml' );
+// Set necessary config class parameter
+$container->setParameter( 'config.configfile', $GLOBALS['phpunit4-config-file-path'] );
+// These service parameters will be used as constructor arguments for pdoEx{}
+$container->setParameter( 'pdoEx.hostname', $GLOBALS['database_host'] );
+$container->setParameter( 'pdoEx.username', $GLOBALS['database_user'] );
+$container->setParameter( 'pdoEx.pass', $GLOBALS['database_password'] );
+$container->setParameter( 'pdoEx.dbname', $GLOBALS['database_name'] );

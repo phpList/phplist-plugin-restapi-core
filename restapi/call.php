f5c29d78c8573181578a75d311e1e8b49dff039f
Sam Tuke <mail@samtuke.com>
2015-04-04 14:25:30 +0200

    Added phpList4 configuration overloading and documentation

--- restapi/call.php
+++ restapi/call.php
@@ -37,12 +37,28 @@ $configFilePath = dirname(__FILE__) . '/config.sample.php';
 
 // Set necessary config class parameter
 $container->setParameter( 'config.configfile', $configFilePath );
-// Set service parameters for the database connection
+// Set service parameters for the RAPI database connection
+// NOTE: phpList4 database connection configured elsewhere
 // These service parameters will be used as constructor arguments for pdoEx{}
 $container->setParameter( 'pdoEx.hostname', $GLOBALS['database_host'] );
 $container->setParameter( 'pdoEx.username', $GLOBALS['database_user'] );
 $container->setParameter( 'pdoEx.pass', $GLOBALS['database_password'] );
 $container->setParameter( 'pdoEx.dbname', $GLOBALS['database_name'] );
+
+// Get a phpList4 configuration object so we can configure the database
+$pl4Config = $container->get( 'Config' );
+
+// Set necessasry phpList4 settings from host app globals and save to session
+// NOTE: phpList 4 by default loads its configuration from a config file.
+// When shipped as a library, manual config is not possible, and a default
+// config file is used. To override default config we must set options here
+// using a phpList4 config object so that they're stored in session and override
+// the default config options which will be loaded from file in client code.
+$pl4Config->setRunningConfig( 'DATABASE_USER', $GLOBALS['database_user'] );
+$pl4Config->setRunningConfig( 'DATABASE_PASSWORD', $GLOBALS['database_password'] );
+$pl4Config->setRunningConfig( 'DATABASE_DSN', 'mysql:host=' . $GLOBALS['database_host'] . ';port=3306;dbname=' . $GLOBALS['database_name'] );
+$pl4Config->setRunningConfig( 'TABLE_PREFIX', $GLOBALS['table_prefix'] );
+$pl4Config->setRunningConfig( 'USERTABLE_PREFIX', $GLOBALS['usertable_prefix'] );
 
 if ( function_exists( 'api_request_log' ) )
 {
